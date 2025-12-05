2067b73f9c011ad3c9c745c807294b2841a1c951
shrandonm <shrandonm@users.noreply.github.com>
2025-12-06 00:38:44 +1100

    Add early/late and sync stats to training tool

--- OpenTaiko/src/Shrandy/NoteStats.cs
+++ OpenTaiko/src/Shrandy/NoteStats.cs
@@ -13,12 +13,17 @@ namespace OpenTaiko.Shrandy
 		public int GoodCount { get; set; }
 		public int OkayCount { get; set; }
 		public int BadCount { get; set; }
+		public int EarlyCount { get; set; }
+		public int LateCount { get; set; }
 		public float TotalHitError { get; set; }
+		public float TotalSync { get; set; }
 
 		[JsonIgnore]
 		public int TotalNotes { get { return GoodCount + OkayCount + BadCount; } }
 		[JsonIgnore]
 		public float AverageHitError { get { return TotalNotes > 0 ? TotalHitError / TotalNotes : 0.0f; } }
+		[JsonIgnore]
+		public float AverageSync { get { return TotalNotes > 0 ? TotalSync / TotalNotes : 0.0f; } }
 		[JsonIgnore]
 		public bool IsDFC { get { return OkayCount == 0 && BadCount == 0; } }
 		[JsonIgnore]
@@ -26,9 +31,11 @@ namespace OpenTaiko.Shrandy
 
 		public void OnNoteHit(HitParams hitParams)
 		{
-			int correctTiming = hitParams.Chip.n発声時刻ms;
-			int error = Math.Abs(hitParams.Chip.nLag);
-			TotalHitError += error;
+			int error = hitParams.Chip.nLag;
+			TotalSync += error;
+
+			int absError = Math.Abs(hitParams.Chip.nLag);
+			TotalHitError += absError;
 
 			if (hitParams.JudgeResult == ENoteJudge.Perfect)
 			{
@@ -42,16 +49,32 @@ namespace OpenTaiko.Shrandy
 			{
 				++BadCount;
 			}
+
+			if (hitParams.JudgeResult == ENoteJudge.Perfect || hitParams.JudgeResult == ENoteJudge.Good)
+			{
+				const int threshold = 10;
+				if (error >= threshold)
+				{
+					LateCount++;
+				}
+				else if (error <= -threshold)
+				{
+					EarlyCount++;
+				}
+			}
 		}
 
 		public static NoteStats operator +(NoteStats left, NoteStats right)
 		{
 			return new NoteStats()
 			{
+				EarlyCount = left.EarlyCount + right.EarlyCount,
+				LateCount = left.LateCount + right.LateCount,
 				GoodCount = left.GoodCount + right.GoodCount,
 				OkayCount = left.OkayCount + right.OkayCount,
 				BadCount = left.BadCount + right.BadCount,
 				TotalHitError = left.TotalHitError + right.TotalHitError,
+				TotalSync = left.TotalSync + right.TotalSync,
 			};
 		}
 
@@ -70,13 +93,19 @@ namespace OpenTaiko.Shrandy
 			int totalNotes = TotalNotes;
 			if (totalNotes > 0)
 			{
+				ImGui.Text($"Early: {EarlyCount} ({StringHelpers.GetPercentString(EarlyCount, totalNotes)}%)");
+				ImGui.Separator();
+				ImGui.Text($"Late: {LateCount} ({StringHelpers.GetPercentString(LateCount, totalNotes)}%)");
+				ImGui.Separator();
 				ImGui.Text($"Goods: {GoodCount} ({StringHelpers.GetPercentString(GoodCount, totalNotes)}%)");
 				ImGui.Separator();
 				ImGui.Text($"Okays: {OkayCount} ({StringHelpers.GetPercentString(OkayCount, totalNotes)}%)");
 				ImGui.Separator();
 				ImGui.Text($"Bads: {BadCount} ({StringHelpers.GetPercentString(BadCount, totalNotes)}%)");
 				ImGui.Separator();
-				ImGui.Text($"Average Error: +/- {AverageHitError}ms");
+				ImGui.Text($"Average Error: +/- {AverageHitError:F2}ms");
+				ImGui.Separator();
+				ImGui.Text($"Average Sync: {AverageSync:F2} ms");
 			}
 		}
 	}
