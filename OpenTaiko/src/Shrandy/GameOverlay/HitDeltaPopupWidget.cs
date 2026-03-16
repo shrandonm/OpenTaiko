743c75ebeeda8da795bace252984101053c5a897
shrandonm <shrandonm@users.noreply.github.com>
2026-03-16 18:34:07 +1100

    Made early/late text more obvious

--- OpenTaiko/src/Shrandy/GameOverlay/HitDeltaPopupWidget.cs
+++ OpenTaiko/src/Shrandy/GameOverlay/HitDeltaPopupWidget.cs
@@ -14,38 +14,11 @@ namespace OpenTaiko.Shrandy
 		}
 
 		private TimedConsoleMessage? m_Message;
-		private TimedConsoleMessage? m_Bar;
 
 		public override void Draw()
 		{
 			base.Draw();
 			m_Message?.Draw();
-			m_Bar?.Draw();
-		}
-
-		private FDK.Color4 CalculateColor(int error)
-		{
-			if (error == 0)
-			{
-				return new FDK.Color4(0.0f, 1.0f, 1.0f, 1.0f);
-			}
-
-			const float maxErrorColor = 15.0f;
-			float colorInterpValue = Math.Clamp(error / maxErrorColor, 0.0f, 1.0f);
-
-			float r, g;
-			if (colorInterpValue < 0.5f)
-			{
-				r = colorInterpValue / 0.5f;
-				g = 1.0f;
-			}
-			else
-			{
-				r = 1.0f;
-				g = 1.0f - ((colorInterpValue - 0.5f) / 0.5f);
-			}
-
-			return new FDK.Color4(r, g, 0.0f, 1.0f);
 		}
 
 		public override void OnNoteHit(in HitParams hitParams)
@@ -55,40 +28,32 @@ namespace OpenTaiko.Shrandy
 			{
 				int screenWidth = OpenTaiko.Skin.Resolution[0];
 				int screenHeight = OpenTaiko.Skin.Resolution[1];
-				int textX = (int)(screenWidth * 0.275f);
-				int textY = (int)(screenHeight * 0.17f);
-				int barX = (int)(screenWidth * 0.0575f);
-				int barY = textY + 64;
+				int textX = (int)(screenWidth * 0.3f);
+				int textY = (int)(screenHeight * 0.175f);
 
 				const int durationMs = 500;
-				const float textScale = 1.0f;
-				const float barScale = 1.5f;
+				const float textScale = 1.5f;
 				int delta = hitParams.Chip.nLag;
 
-				string prefix = delta == 0 ? "Perfect!"
-							  : delta > 0  ? "Late "
-										   : "Early";
+				const int errorThresholdMs = 20;
+				bool isLate = delta > errorThresholdMs;
+				bool isEarly = delta < -errorThresholdMs;
+				string message = isLate ? "Late"
+							 : isEarly ? "Early"
+							 	: "";
 
-				const int maxErrorBars = 25;
-				StringBuilder errorBars = new(maxErrorBars);
-				for (int i = maxErrorBars; i >= -maxErrorBars; --i)
+				FDK.Color4 color = isLate ? new FDK.Color4(1.0f, 0.75f, 0.0f, 1.0f) // Orange
+							 : isEarly ? new FDK.Color4(0.0f, 0.75f, 1.0f, 1.0f) // Blue
+							 : new FDK.Color4(1.0f, 1.0f, 1.0f, 1.0f); // White
+							 
+				if (isLate || isEarly)
 				{
-					bool addBar = (delta < 0 && i < 0 && i >= delta) || (delta > 0 && i > 0 && i <= delta);
-					errorBars.Append(addBar ? '|' : ' ');
-				}
-
-				FDK.Color4 color = CalculateColor(Math.Abs(delta));
-				m_Message = new(textX, textY,
-					CTextConsole.EFontType.White, $"{prefix} {delta:+#;-#;0}",
+					m_Message = new(textX, textY,
+					CTextConsole.EFontType.White, message,
 					durationMs,
 					textScale,
 					color);
-
-				// m_Bar = new(barX, barY,
-					// CTextConsole.EFontType.White, $"{errorBars.ToString()}",
-					// durationMs,
-					// barScale,
-					// color);
+				}
 			}
 		}
 	}
