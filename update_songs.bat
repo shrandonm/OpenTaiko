99478036fef12f228c5cc2570e39950b48ab76ee
shrandonm <shrandonm@users.noreply.github.com>
2026-07-22 00:15:53 +1000

    Add custom charts to main update songs script

--- update_songs.bat
+++ update_songs.bat
@@ -2,5 +2,11 @@ cd "OpenTaiko\bin\Release\net8.0\win-x64\Songs"
 git clone https://ese.tjadataba.se/ESE/ESE.git ESE
 cd ESE
 git pull
+
+cd "..\"
+git clone https://github.com/shrandonm/CustomTaikoCharts.git ShrandyCustomCharts
+cd ShrandyCustomCharts
+git pull
+
 echo Update completed!
 pause
--- update_training_songs.bat
+++
@@ -1,6 +0,0 @@
-cd "OpenTaiko\bin\Release\net8.0\win-x64\Songs"
-git clone https://github.com/shrandonm/CustomTaikoCharts.git TrainingCharts
-cd TrainingCharts
-git pull
-echo Update completed!
-pause
